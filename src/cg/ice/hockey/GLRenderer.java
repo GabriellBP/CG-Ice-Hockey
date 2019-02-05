1bb42b8e32caf556545e801723d7afaed0fa4cbe
danihcv <daniel.vassalo@hotmail.com.br>
2019-02-04 21:57:30 -0300

    adiciona estrutura final do projeto

--- src/cg/ice/hockey/GLRenderer.java
+++ src/cg/ice/hockey/GLRenderer.java
@@ -1,5 +1,7 @@
 package cg.ice.hockey;
 
+import cg.ice.hockey.strategies.line.LineStrategy;
+import cg.ice.hockey.util.Point;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
@@ -7,14 +9,22 @@ import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.util.gl2.GLUT;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
+import java.util.ArrayList;
 
 class GLRenderer implements GLEventListener {
     private GLU glu;
     private GL2 gl;
-    private int width, height;
-    private boolean ready = false;
+    private final int width, height;
+    
+    private int stage = 0;
+    private ArrayList<Point> arenaPoints = new ArrayList();
+    
+    private ArenaRenderer arenaRenderer;
+    private BleachersRenderer bleachersRenderer;
 
     GLRenderer(GLU glu, int width, int height) {
+        this.arenaRenderer = new ArenaRenderer();
+        this.bleachersRenderer = new BleachersRenderer();
         this.glu = glu;
         this.width = width;
         this.height = height;
@@ -37,8 +47,9 @@ class GLRenderer implements GLEventListener {
     @Override
     public void display(GLAutoDrawable drawable) {
         System.out.println("display");
-        if (ready)
-            drawCircle(20, width / 2, height / 2);
+        if (stage > 0) {
+            arenaRenderer.drawArena(arenaPoints.get(0), arenaPoints.get(1));
+        }
     }
 
     @Override
@@ -47,24 +58,12 @@ class GLRenderer implements GLEventListener {
     }
 
     void onClick(int x, int y) {
-        ready = true;
-    }
-    
-    void drawCircle(float radius, float x, float y) {
-        System.out.println("drawing circle");
-        gl.glPushMatrix();
-            gl.glTranslated(x, y, 0);
+        if (stage == 0) { // esperando pontos da arena
+            this.arenaPoints.add(new Point(x, y));
             
-            gl.glBegin(gl.GL_LINES);
-            for (int i = 0; i < 360; i++) {
-                gl.glColor3f(139, 69, 19);
-                x = (float) (radius * cos(i));
-                y = (float) (radius * sin(i));
-                gl.glVertex3f(0, 0, 0);
-                gl.glVertex3f(x, y, 0);
+            if (this.arenaPoints.size() == 2) {
+                stage++;
             }
-            gl.glEnd();
-        gl.glPopMatrix();
-        gl.glFlush();
-}
+        }
+    }
 }
