7194688a4b1fa82607e84b9d68473d7eb4c23c88
GabriellBP <gbp@ic.ufal.br>
2019-02-04 23:09:52 -0300

    Ajustes de tipos, encapsulamento e de nomenclatura

--- src/cg/ice/hockey/util/Circle.java
+++ src/cg/ice/hockey/util/Circle.java
@@ -2,10 +2,10 @@ package cg.ice.hockey.util;
 
 import java.util.ArrayList;
 
-public class Cirlce {
-    ArrayList<Point> points = new ArrayList();
+public class Circle {
+    public ArrayList<Point> points = new ArrayList();
     
-    void addPoint(int x, int y) {
+    public void addPoint(int x, int y) {
         this.points.add(new Point(x, y));
     }
 }
--- src/cg/ice/hockey/util/Line.java
+++ src/cg/ice/hockey/util/Line.java
@@ -3,9 +3,9 @@ package cg.ice.hockey.util;
 import java.util.ArrayList;
 
 public class Line {
-    ArrayList<Point> points = new ArrayList();
+    public ArrayList<Point> points = new ArrayList();
     
-    void addPoint(int x, int y) {
+    public void addPoint(int x, int y) {
         this.points.add(new Point(x, y));
     }
 }
