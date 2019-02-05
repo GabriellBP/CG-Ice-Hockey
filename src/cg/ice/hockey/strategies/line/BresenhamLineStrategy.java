2b6e76c31695beb7fa1ffc06ba72164e329955a8
GabriellBP <gbp@ic.ufal.br>
2019-02-05 17:04:00 -0300

    Correção e reformulação do algoritmo de Bresenham para desenho de linha

--- src/cg/ice/hockey/strategies/line/BresenhamLineStrategy.java
+++ src/cg/ice/hockey/strategies/line/BresenhamLineStrategy.java
@@ -2,78 +2,39 @@ package cg.ice.hockey.strategies.line;
 
 import cg.ice.hockey.util.Point;
 import cg.ice.hockey.util.Line;
+import static java.lang.Math.abs;
 
 public class BresenhamLineStrategy implements LineStrategy {
 
     @Override
-    public Line generateLine(Point pp1, Point pp2) {
-        int limit;
-        Point p1, p2;
-        if (pp1.x == pp2.x) {
-            if (pp1.y < pp2.y) { // Norte
-                limit = pp2.y;
-            } else { // Sul
-                limit = pp1.y;
-            }
-        } else {
-            if (pp1.x < pp2.x) {
-                limit = pp2.x;
-            } else {
-                limit = pp1.x;
-            }
-        }        
-        
-        if (pp1.x > pp2.x) {
-            p1 = new Point(pp2.x, pp2.y);
-            p2 = new Point(pp1.x, pp1.y);
-            System.out.println("INVERTEU CACHORRA");
-        } else {
-            p1 = new Point(pp1.x, pp1.y);
-            p2 = new Point(pp2.x, pp2.y);
-        }
-
-
-        /*
-            x1 < x2 e y1 < y2 [NE]
-            x1 < x2 e y1 > y2 [SE] <-
-            x1 > x2 e y1 < y2 [NW] <-
-            x1 > x2 e y1 > y2 [SW] <-
-            
-            x1 = x2 <- x em função de y
-                y1 < y2 [N]
-                y1 > y2 [S]
-            y1 = y2
-                x1 < x2 [E]
-                x1 > x2 [W]
-        */
-        
-        int dx = p2.x - p1.x;
-        int dy = p2.y - p1.y;
-        
-        int d = 2 * dy - dx;  // valor inicial de D
-        
-        int deltaE = 2 * dy;  // incremento de E
-        int deltaNE = 2 * (dy - dx);  // incremento de NE
-                
-        int x = p1.x;
-        int y = p1.y;
-        
+    public Line generateLine(Point p1, Point p2) {
+        int dx, dy, x, y, incX, incY, abs;
         Line line = new Line();
+        
+        x = p1.x;
+        y = p1.y;
+        
         line.addPoint(x, y);
         
-        while (x <= p2.x) {
-            if (d <= 0) {
-                /* escolhe E */
-                d += deltaE;
-                x++;
-            } else {
-                System.out.println("INCREMENTOU X E Y");
-                /* escolhe NE */
-                d += deltaNE;
-                x++;
-                y++;
+        dx = Math.abs(p2.x - x);
+        dy = Math.abs(p2.y - y);
+        abs = dx - dy;
+
+        incX = (x < p2.x) ? 1 : -1;
+        incY = (y < p2.y) ? 1 : -1;
+
+        while ((x != p2.x) || (y != p2.y)) {  
+            int p = 2 * abs;
+
+            if (p > -dy) {
+                abs = abs - dy;
+                x = x + incX;
             }
-            line.addPoint(x, y);
+            if (p < dx) {
+                abs = abs + dx;
+                y = y + incY;
+            }
+             line.addPoint(x, y);
         }
         
         return line;
