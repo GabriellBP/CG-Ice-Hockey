7d4f0feecb6569a9f4f43d34121ac63a6b63c275
GabriellBP <gbp@ic.ufal.br>
2019-02-05 23:25:00 -0300

    Implementação do Bresenham para o desenho das circunferências

--- src/cg/ice/hockey/strategies/circle/BresenhamCircleStrategy.java
+++ src/cg/ice/hockey/strategies/circle/BresenhamCircleStrategy.java
@@ -7,36 +7,61 @@ public class BresenhamCircleStrategy implements CircleStrategy {
 
     @Override
     public Circle generateCircle(Point center, int radius) {
-        Circle circle = new Circle(center, radius);
+        Circle fullCircle = new Circle(center, radius);
+        
+        fullCircle.points.addAll(generateQuadrant(center, radius, 1).points);
+        fullCircle.points.addAll(generateQuadrant(center, radius, 2).points);
+        fullCircle.points.addAll(generateQuadrant(center, radius, 3).points);
+        fullCircle.points.addAll(generateQuadrant(center, radius, 4).points);
+        
+        return fullCircle;
+    }
+
+    @Override
+    public Circle generateQuadrant(Point center, int radius, int code) {
+        Circle quadrant = new Circle(center, radius);
         
         int x = 0;
         int y = radius;
         float d = (5/4) - radius;
         
-        circle.addPoint(x, y);
+        quadrant.addPoint(x, y);
         while (y > x) {
             if (d < 0) {
                 /* escolhe E */
                 d = d + 2 * x + 3;
                 x++;
             } else {
-                /* escolhe NE */
+                /* escolhe SE */
                 d = d + 2 * (x - y) + 5;
                 x++;
                 y--;
             }
-            circle.addPoint(x, y);
+            if (code == 1) {
+                quadrant.addPoint(center.x + x, center.y + y);
+                quadrant.addPoint(center.x + y, center.y + x);
+            } else if (code == 2) {
+                quadrant.addPoint(center.x - x, center.y + y);
+                quadrant.addPoint(center.x - y, center.y + x);
+            } else if (code == 3) {
+                quadrant.addPoint(center.x - x, center.y - y);
+                quadrant.addPoint(center.x - y, center.y - x);
+            } else {
+                quadrant.addPoint(center.x + y, center.y - x);
+                quadrant.addPoint(center.x + x, center.y - y);
+            }
         }
-        return circle;
-    }
-
-    @Override
-    public Circle generateQuadrant(Point center, int radius, int code) {
-        return new Circle(center, radius);
+        return quadrant;
     }
 
     @Override
     public Circle generateQuadrant(Point center, int radius, int[] codes) {
-        return new Circle(center, radius);
+        Circle circle = new Circle(center, radius);
+        
+        for (int i = 0; i < codes.length; i++) {
+            circle.points.addAll(generateQuadrant(center, radius, codes[i]).points);
+        }
+        
+        return circle;
     }
 }
